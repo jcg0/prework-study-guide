c992c32a6e7d646883e2a2f0a695068cdcfd5fb7
jcg0 <jg.awsprod@gmail.com>
2022-09-25 14:45:05 -0400

    Added a script file containing a conditional if statement and a for loop

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+for (let i = 0; i < topics.length; i++) {
+  console.log(topics[i]);
+}
+
+// if (topics === "HTML") {
+//   console.log("lets study HTML!");
+// } else if (topics === "CSS") {
+//   console.log("lets study CSS!");
+// } else if (topics === "Git") {
+//   console.log("lets study Git!");
+// } else if (topics === "JavaScript") {
+//   console.log("lets study JavaScript!");
+// } else {
+//   console.log("please try again!");
+// }
