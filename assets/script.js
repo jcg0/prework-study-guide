dcbab8fb4c3a52453971d3d7f1e51c8795d389dc
jcg0 <jg.awsprod@gmail.com>
2022-09-25 15:31:23 -0400

    created two functions to call my if statement and for loop

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,27 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
-for (let i = 0; i < topics.length; i++) {
-  console.log(topics[i]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+  for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+  }
 }
 
-// if (topics === "HTML") {
-//   console.log("lets study HTML!");
-// } else if (topics === "CSS") {
-//   console.log("lets study CSS!");
-// } else if (topics === "Git") {
-//   console.log("lets study Git!");
-// } else if (topics === "JavaScript") {
-//   console.log("lets study JavaScript!");
-// } else {
-//   console.log("please try again!");
-// }
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("lets study HTML!");
+  } else if (randomTopic === "CSS") {
+    console.log("lets study CSS!");
+  } else if (randomTopic === "Git") {
+    console.log("lets study Git!");
+  } else if (randomTopic === "JavaScript") {
+    console.log("lets study JavaScript!");
+  } else {
+    console.log("please try again!");
+  }
+}
+
+console.log("Here are the topics we learned through Prework: ");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
